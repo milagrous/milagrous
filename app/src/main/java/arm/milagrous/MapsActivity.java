a87f869498e5209ee874515f3d4f8ec5dc370506
Movses Saponjyan <movses.saponjyan@gmail.com>
2016-10-30 10:55:10 +0400

    skeleton popup on long click when achieved max zoom

--- app/src/main/java/arm/milagrous/MapsActivity.java
+++ app/src/main/java/arm/milagrous/MapsActivity.java
@@ -1,22 +1,43 @@
 package arm.milagrous;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
-
+import android.view.ViewGroup.LayoutParams;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+
 
 import arm.milagrous.dbConnection.Record;
 import arm.milagrous.dbConnection.RecordCRUD;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnMapLongClickListener {
 
     private GoogleMap mMap;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +47,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
-
         // test mock
 /*
         final RecordCRUD crud = RecordCRUD.getInstance(this);
@@ -49,6 +67,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
 */
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -62,12 +83,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
 
         // Add a marker in Sydney and move the camera
-        LatLng stepanakert = new LatLng(39.8264417,46.728725);
-        mMap.addMarker(new MarkerOptions().position(stepanakert).title("Ստեփանակերտ"));
+        LatLng stepanakert = new LatLng(39.8264417, 46.728725);
+        Marker initialMarker = mMap.addMarker(new MarkerOptions().position(stepanakert).title("Ստեփանակերտ"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(stepanakert));
+
+    }
+
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+
+            float maxZoomLevel = mMap.getMaxZoomLevel();
+            float curZoomLevel = mMap.getCameraPosition().zoom;
+
+            if (curZoomLevel == maxZoomLevel) {
+                double latitude = latLng.latitude;
+                double longitude = latLng.longitude;
+
+                LayoutInflater layoutInflater
+                        = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                View popupView = layoutInflater.inflate(R.layout.record_popup, null, false);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT);
+
+
+                Button btnCancel = (Button)popupView.findViewById(R.id.cancel);
+                btnCancel.setOnClickListener(new Button.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+                popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+            }
+
     }
 }
---
+++ app/src/main/java/arm/milagrous/RecordPopupActivity.java
@@ -0,0 +1,38 @@
+/*package arm.milagrous;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+public class RecordPopupActivity extends FragmentActivity {
+    // Called when the activity is first created.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        View popupView = LayoutInflater.from(getBaseContext()).inflate(R.layout.record_popup, null);
+
+        final PopupWindow popupWindow = new PopupWindow(
+                popupView,
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+
+
+//        Button btnDismiss = (Button)popupView.findViewById(R.id.cancel);
+//        btnDismiss.setOnClickListener(new Button.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                popupWindow.dismiss();
+//            }
+//        });
+
+
+
+    }
+}*/
